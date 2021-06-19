0f750b2f38ddff3da34dd235fb3ffdb1edcebdc6
d4nieldev <danielohayon2019@gmail.com>
2021-06-19 11:54:16 +0300

    finished menu design

--- core/models.py
+++ core/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 class MenuOption(models.Model):
-    parent = models.ForeignKey('MenuOption', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
+
+    def get_children(self):
+        all_options = MenuOption.objects.all()
+        children = []
+
+        print(all_options)
