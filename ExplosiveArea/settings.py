1a76c26aff281c0a6e23f8c1de28a63ff10c4bef
d4nieldev <danielohayon2019@gmail.com>
2021-07-08 23:01:38 +0300

    server

--- ExplosiveArea/settings.py
+++ ExplosiveArea/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,7 +86,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': str(os.environ.get('POSTGRESQL_PASSWORD')),
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '5433',
     }
 }
 
