6a1ee11d20f0eb7b4d98cc0347bf6e6406d90d6d
d4nieldev <danielohayon2019@gmail.com>
2021-07-09 09:12:42 +0300

    database

--- ExplosiveArea/settings.py
+++ ExplosiveArea/settings.py
@@ -78,16 +78,29 @@ WSGI_APPLICATION = 'ExplosiveArea.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'explosive_zone',
-        'USER': 'postgres',
-        'PASSWORD': str(os.environ.get('POSTGRESQL_PASSWORD')),
-        'HOST': 'localhost',
-        'PORT': '5433',
+
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'explosive_zone',
+            'USER': 'postgres',
+            'PASSWORD': str(os.environ.get('POSTGRESQL_PASSWORD')),
+            'HOST': 'localhost',
+            'PORT': '5433',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgresql-fitted-31314',
+            'USER': 'huojacnjqbhcge',
+            'PASSWORD': "ba1e46127fe1237a2e13ac195fdbeb10ae036b7119bab5f1630260bbd5f0e435",
+            'HOST': 'ec2-54-74-35-87.eu-west-1.compute.amazonaws.com',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
